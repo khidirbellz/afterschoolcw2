cda45911dac6bd3823d69f10645902303f4b8780
khidirbellz <95444763+khidirbellz@users.noreply.github.com>
2022-01-18 13:07:58 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 //import express from node modules
 const exp = require("express");
+// make express app
+const app = exp();
+//use express
+app.use(exp.json());
+//tell express where to get html clss and jss files
+app.use(exp.static('public'));
+//use the https headers
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Headers",'Origin,X-requested-With,Content-Type,Accept');
+    response.setHeader('Access-Control-Allow-Methods','PUT');
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    next();
+});
